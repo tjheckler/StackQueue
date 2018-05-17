24980b2c626e99c380775e3be7945dd9a5fba7c0
tjheckler <timothyheckler@gmail.com>
2018-05-17 03:33:55 -0500

    StackQueue Task Helper Silver

--- src/com/company/TaskHelper.java
+++ src/com/company/TaskHelper.java
@@ -37,22 +37,50 @@ public class TaskHelper
             command = commands[0].toUpperCase();
             try
             {
-                if (command.equals("ADD") && commands.length == 2)
+                // could be in method
+                if (command.equals("ADD") && commands.length >= 2)
                 {
-                    String task = commands[1];
-                    addTask(task);
+                    for (int i = 0; i < commands.length; i++)
+                    {
+                        String task = commands[i];
+                        addTask(task);
 
+                        if (myQueue.contains("add"))
+                        {
+                            myQueue.remove();
+                        }
+                    }
                 }
-
+                // redundant after method created
                 if (command.equals("PEEK"))
                 {
-                    peek();
+                    System.out.println(myQueue.peek());
+                }
+                //uses method
+                if (command.equals("PEEK") && commands.length == 2)
+                {
+                    int n = Integer.valueOf(commands[1]);
+                    peekTask(n, myQueue);
                 }
 
+                if (command.equals("REMOVE") && commands.length == 2)
+                {
+                    // could be in  method
+                    int n = Integer.parseInt(commands[1]);
+                    for (int i = 0; i < n - 1; i++)
+                    {
+
+                        myQueue.remove(n);
+                        System.out.println("Removed " + myQueue.remove());
+
+                    }
+
+                }
                 if (command.equals("REMOVE"))
                 {
                     removeTask();
                 }
+
                 if (command.equals("HOWMANY"))
                 {
                     howMany();
@@ -75,15 +103,42 @@ public class TaskHelper
         myQueue.add(task);
     }
 
-    private void peek()
+    private void peekTask(int n, Queue<String> buffer)
     {
-        System.out.println(myQueue.peek());
+
+        // dirty but works, hardest part of project
+        if (myQueue.size() == 1 && n == 0)
+            System.out.println("The next item on the list is " + myQueue.peek());
+
+        else if (myQueue.size() == 0)
+        {
+            System.out.println("List is empty, add items first");
+        } else if (myQueue.size() > 1 && n <= myQueue.size())
+        {
+            buffer = new LinkedList<>();
+
+            for (String task : myQueue)
+            {
+                buffer.add(task);
+            }
+            System.out.println("The next " + n + " items on the list ");
+
+            for (int i = 0; i < n; i++)
+            {
+                System.out.println(buffer.poll());
+            }
+        } else if (n > myQueue.size())
+        {
+            System.out.println("Try a lesser number");
+        } else
+            System.out.println("List is empty");
     }
+
 
     private void removeTask()
     {
-        System.out.println(myQueue.remove());
-        System.out.println(myQueue.peek());
+        System.out.println("Removed " + myQueue.remove());
+        System.out.println("Next on list is " + myQueue.peek());
     }
 
     private void howMany()
